49c254b305e10720371f9a1cf12db6776c2b7f3c
playcsgo <sam@plum.wine>
2023-05-30 17:00:48 +0800

    feat: create service for getRestaurants

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,43 +1,10 @@
 const { Restaurant, Category, Comment, User } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+
+const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantColler = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset,
-        raw: true,
-        nest: true
-      }),
-      Category.findAll({ raw: true }) // 為了render category-Nav
-    ])
-      .then(([restaurants, categories]) => {
-        const favoratedRestaurantsId = req.user.FavoritedRestaurants.map(fr => fr.id)
-        const likedRestaurantsId = req.user.LikedRestaurants.map(lr => lr.id)
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: favoratedRestaurantsId.includes(r.id), // 這樣data裡面就有isFavorated這個boolean值
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.counts)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
