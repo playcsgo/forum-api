e5e748fd681c1472d22cd93c0d2e8a16d140a732
playcsgo <sam@plum.wine>
2023-05-31 09:47:19 +0800

    feat: add jwt for signIn function

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON()
+      delete userData.password
+      // JWT (A, B, C)
+      // A: payload, 要打包的資訊; B: 密鑰, 使用.env來寫; C: 設定, 這邊設定有效天數
+      // process.env.JWT_SECRET
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+module.exports = userController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -4,11 +4,19 @@ const router = express.Router()
 const restController = require('../../controllers/apis/restaurant-controller')
 const admin = require('./modules/admin')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const passport = require('../../config/passport')
+const userController = require('../../controllers/apis/user-controller')
 
+// admin
 router.use('/admin', admin)
 
+// User
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 新增這行，設定 session: false
+
+// Restaurant
 router.get('/restaurants', restController.getRestaurants)
 
+// admin Error Handler
 router.use('/', apiErrorHandler)
 
 module.exports = router
