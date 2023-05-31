a71afae6af9c01a0e2258c6ebd7e76202d6041a5
playcsgo <sam@plum.wine>
2023-05-31 10:41:02 +0800

    feat: add jwt for auth

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -6,15 +6,16 @@ const admin = require('./modules/admin')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const passport = require('../../config/passport')
 const userController = require('../../controllers/apis/user-controller')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 
 // admin
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 
 // User
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 新增這行，設定 session: false
 
 // Restaurant
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 // admin Error Handler
 router.use('/', apiErrorHandler)
