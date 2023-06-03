f4acb1dda591889145fd9e27ac7264ab65c94d54
playcsgo <sam@plum.wine>
2023-06-03 14:12:36 +0800

    api getFeeds-2

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category, Comment, User } = require('../../models')
+const { Restaurant, User } = require('../../models')
 
 const restaurantServices = require('../../services/restaurant-services')
 
@@ -21,26 +21,6 @@ const restaurantColler = {
   },
   getFeeds: (req, res, next) => {
     restaurantServices.getFeeds(req, (err, data) => err ? next(err) : res.render('feeds', { restaurants: data.restaurants, comments: data.comments }))
-    // return Promise.all([
-    //   Restaurant.findAll({
-    //     limit: 10,
-    //     order: [['createdAt', 'DESC']], // DESC ASC
-    //     include: [Category],
-    //     raw: true,
-    //     nest: true
-    //   }),
-    //   Comment.findAll({
-    //     limit: 10,
-    //     order: [['createdAt', 'DESC']],
-    //     include: [User, Restaurant],
-    //     raw: true,
-    //     nest: true
-    //   })
-    // ])
-    //   .then(([restaurants, comments]) => {
-    //     res.render('feeds', { restaurants, comments })
-    //   })
-    //   .catch(err => next(err))
   },
   getTopRestaurants: (req, res, next) => {
     return Restaurant.findAll({
